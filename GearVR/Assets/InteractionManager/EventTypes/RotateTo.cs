b969d89449a13d64853c5eded1d32417fd04e88d
mahdafr <mmafravi@gmail.com>
2018-08-16 13:35:06 -0600

    created projects

---
+++ GearVR/Assets/InteractionManager/EventTypes/RotateTo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RotateTo : EventIM
+{
+    /// <summary>
+    /// TODO: Update this with a proper rotation system that blends the rotating animation properly.
+    /// </summary>
+    [Tooltip("Mandatory. Rotate the agent's body to look at this object.")]
+    public GameObject rotateToObject;
+    [Tooltip("The speed in which you would like the agent to rotate at.")]
+    public float speed;
+    //protected EventType eventType = EventType.RotateTo;
+}
